f7e86dd4d82e495c581a49501e1b81051477010a
Borislav <borislav2003@gmail.com>
2022-10-12 19:14:46 +0500

    Player model

--- src/players/player.model.ts
+++ src/players/player.model.ts
@@ -1,7 +1,35 @@
-import {Model, Table} from "sequelize-typescript";
+import {Column, DataType, Index, Model, Table} from "sequelize-typescript";
 import {CreatePlayerDto} from "./dto/create-player.dto";
 
 @Table({tableName: 'players'})
 export class PlayerModel extends Model<PlayerModel, CreatePlayerDto> {
 
+  @Column({type: DataType.NUMBER, unique: true, primaryKey: true, autoIncrement: true})
+  id: number;
+
+  @Index({
+    name: 'fullName',
+    unique: true
+  })
+  @Column({type: DataType.STRING, allowNull: false})
+  firstname: string;
+
+  @Index({
+    name: 'fullName',
+    unique: true
+  })
+  @Column({type: DataType.STRING, allowNull: false})
+  lastname: string;
+
+  @Column({type: DataType.STRING, allowNull: false})
+  sex: string;
+
+  @Column({type: DataType.DATE, allowNull: false})
+  birthday: Date;
+
+  @Column({type: DataType.STRING, allowNull: false})
+  teamName: string;
+
+  @Column({type: DataType.STRING, allowNull: false})
+  country: string;
 }
